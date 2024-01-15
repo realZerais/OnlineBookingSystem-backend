2973134d90e29c3bc7d27cc526b5db6900f2935f
zerais <davesiarez18@gmail.com>
2024-01-15 10:44:39 +0800

    login added with no cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,20 +2,32 @@ const jwt = require("jsonwebtoken");
 const db = require("../config/db");
 const bcrypt = require('bcryptjs');
 
-
+const generateToken = (_username) => {
+    return jwt.sign({ _username }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
     const { username, password } = req.body;
     try {
 
         const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-        const user = result.rows[0];
+        // const user = result.rows[0];
 
-        if (!user || password != user.password) {
-            return res.status(401).send('Invalid username or password');
+        // Check if user exist
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid username or password' });
         }
-        const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
-        res.send({ token });
+
+        // Compare the hashed password
+        const isValidPassword = await bcrypt.compare(password, result.rows[0].password);
+
+        if (!isValidPassword) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const token = generateToken(username);
+
+        res.status(200).json({ username, token });
 
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -28,6 +40,7 @@ const signupUser = async (req, res) => {
     const { username, password, email, full_name, phone_number } = req.body;
 
     const query = 'INSERT INTO users (username, password, email, full_name, phone_number, user_role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ';
+
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const result = await db.query(query, [username, hashedPassword, email, full_name, phone_number, user_role]);
